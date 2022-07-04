e25f79ce4ae270c1c6bdcb679dec9a1ba0a10d10
Junior Developer <lotifurshabbir54@gmail.com>
2022-07-04 22:30:38 +0600

    many features added

--- index.js
+++ index.js
@@ -174,16 +174,16 @@ workObserver.observe(workSection);
 
 // our popup section 
 
-window.addEventListener("load", function () {
-  setTimeout(
-    function open(event) {
-      document.querySelector(".popup").style.display = "block";
-    },
-    1000
-  )
-});
+// window.addEventListener("load", function () {
+//   setTimeout(
+//     function open(event) {
+//       document.querySelector(".popup").style.display = "block";
+//     },
+//     1000
+//   )
+// });
 
 
-document.querySelector("#close").addEventListener("click", function () {
-  document.querySelector(".popup").style.display = "none";
-});
+// document.querySelector("#close").addEventListener("click", function () {
+//   document.querySelector(".popup").style.display = "none";
+// });
